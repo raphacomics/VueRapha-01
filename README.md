b0dc32ecba8e67ae742433d2325a714defae923f
raphacomics <83170908+raphacomics@users.noreply.github.com>
2023-02-22 12:56:23 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# VueRapha-01
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-5a2rnp)
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+
+const addCount = () => {
+  count.value = count.value + 1;
+};
+const removeCount = () => {
+  if (count.value <= 0) {
+    return;
+  } else {
+    count.value = count.value - 1;
+  }
+};
+</script>
+
+<template>
+  <div class="container text-center">
+    <p class="display-3">{{ count }}</p>
+    <div class="d-grid gap-2">
+      <button @click="addCount" class="btn btn-primary">ADD</button>
+      <button @click="removeCount" class="btn btn-primary">REMOVE</button>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
